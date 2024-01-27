b300f5b60e7a8ac6fa0c99a7be434b9edc13679c
Matheus Nunes <matheus.nunes@ebanx.com>
2024-01-27 12:56:10 -0300

    changes

--- math_test.go
+++ math_test.go
@@ -10,3 +10,12 @@ func TestSoma(t *testing.T) {
 		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 30)
 	}
 }
+
+func TestSub(t *testing.T) {
+
+	total := sub(15, 15)
+
+	if total != 0 {
+		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 0)
+	}
+}
